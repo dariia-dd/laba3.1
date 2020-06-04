8fe903de44780de788782720fb12216ff937882a
dariia-dd <61156730+dariia-dd@users.noreply.github.com>
2020-06-04 13:16:14 +0300

    Add files via upload

---
+++ OOP_lab_3_9_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace OOP_lab_3_9_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader fromFile = new StreamReader("input.txt");
+
+            string str = fromFile.ReadToEnd();
+
+            string[] elements = str.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+
+            int[] array = new int[elements.Length];
+
+            int index = 0;
+
+            foreach (string element in elements)
+            {
+                array[index] = element.Length;
+
+                ++index;
+            }
+
+            for (int i = 0; i < array.Length; ++i)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
